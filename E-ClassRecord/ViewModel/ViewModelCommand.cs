381129562a3d2dfb678559e75e14d6dc953eb16b
Paul Sho <Cyberkiller.CK@gmail.com>
2023-04-04 23:33:19 +0800

    Update

--- E-ClassRecord/ViewModel/ViewModelCommand.cs
+++ E-ClassRecord/ViewModel/ViewModelCommand.cs
@@ -10,17 +10,41 @@ namespace E_ClassRecord.ViewModel
 {
     public class ViewModelCommand : ICommand
     {
-        //Field of type action
-        public event EventHandler CanExecuteChanged;
+        //Fields
+        private readonly Action<object> _executeAction;
+        private readonly Predicate<object> _canExecuteAction;
 
+        //Constructor
+
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = null;
+        }
+
+        public ViewModelCommand(Action<object> executeAction, Predicate<object> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+
+        // Events
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        //Methods
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return _canExecuteAction == null ? true : _canExecuteAction(parameter);
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            _executeAction(parameter);
         }
     }
 }
